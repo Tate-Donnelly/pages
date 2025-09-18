76fe4d385ad8bc6e7b2f914f7464a3019573ab53
tate-donnelly <tate.donne@gmail.com>
2025-09-18 00:29:40 -0400

    Start menu css and updating

--- ViteTest/src/components/Desktop/StartMenu.jsx
+++ ViteTest/src/components/Desktop/StartMenu.jsx
@@ -3,15 +3,15 @@ import "../../styles/start-menu.css";
 export default function StartMenu({visible, onOpenWindow, onClose}) {
     if (!visible) return null;
     return (
-        <div className="start-menu" onMouseDown={(e) => e.stopPropagation()} role="dialog" aria-label="Start menu">
-            <div className="start-menu-left">Windows&nbsp;XP</div>
+        <div className="start-menu" onMouseDown={(e) => e.stopPropagation()} role="dialog" aria-label="Menu">
+            <div className="start-menu-left">Hi, I'm Tate. Welcome to my portfolio!</div>
             <div className="start-menu-right">
                 <button className="start-item" onClick={() => onOpenWindow("projects")}>Projects</button>
                 <button className="start-item" onClick={() => onOpenWindow("about-me")}>About Me</button>
                 <button className="start-item" onClick={() => onOpenWindow("github")}>GitHub</button>
                 <button className="start-item" onClick={() => onOpenWindow("linkedin")}>Linkedin</button>
                 <hr/>
-                <button className="start-item" onClick={onClose}>Close Menu</button>
+                <button className="start-item" onClick={onClose}>Close</button>
             </div>
         </div>
     );
